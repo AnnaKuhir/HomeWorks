1a0d82b4b95dc43d641f50fbc57949c1341fc840
Anna <anna.kuhir@nure.ua>
2020-01-21 15:11:34 +0200

    Changed task 5

---
+++ HW10/main.js
@@ -0,0 +1,83 @@
+
+class Condidate {
+  constructor(condidate) {
+    Object.assign(this, condidate);
+  }
+  get state() {
+    return this.address.split(',').slice(2, 3).join();
+  }
+}
+
+const condidate = new Condidate(condidateArr[0])
+
+console.log('------------------------------------------1-----------------------------------------')
+console.log(condidate.state)
+
+console.log('------------------------------------------2-----------------------------------------')
+let getNamesCompany = () => {
+  let companyNames = [];
+  for (let key in condidateArr){
+    if(!companyNames.includes(condidateArr[key].company)){
+    companyNames.push(condidateArr[key].company)
+  }
+}
+  return companyNames;
+}
+console.log(getNamesCompany());
+
+console.log('------------------------------------------2.1-----------------------------------------')
+const companyArray = condidateArr.map((item) => {
+  return item.company
+});
+const newArray = [...new Set(companyArray)];
+console.log(newArray);
+
+console.log('------------------------------------------2.2-----------------------------------------')
+const newArray1 = companyArray.filter((item, index) => index === companyArray.indexOf(item));
+console.log(newArray1);
+
+console.log('------------------------------------------3-----------------------------------------')
+let getUserByYear = year => {
+  let result = [];
+  for (let key in condidateArr){
+    if (Number (condidateArr[key].registered.split('-')[0]) === year){
+      result.push(condidateArr[key]._id)
+    }
+  }
+  return result;
+}
+console.log(getUserByYear(2019));
+//3.1
+let getUsersByYear = year =>{
+  let getUser = [];
+  condidateArr.map(function(item){
+    if (Number(item.registered.split('-')[0]) === year){
+      getUser.push(item._id)
+    };
+  });
+  return getUser;
+}
+console.log(getUsersByYear(2019)); 
+
+console.log('------------------------------------------4-----------------------------------------')
+let getCondidatesByUnreadMsg = num => {
+  let getMessage = []
+  condidateArr.map((item) => {
+      if (+item.greeting.split('!')[1].trim().split(' ')[2] === num) {
+          getMessage.push(item)
+      }
+  })
+  return getMessage
+}
+console.log(getCondidatesByUnreadMsg(10));
+
+
+console.log('------------------------------------------5-----------------------------------------')
+let getCondidatesByGender = gender => {
+  return condidateArr.filter(item => item.gender === gender);
+}
+console.log(getCondidatesByGender('male'));
+
+
+
+
